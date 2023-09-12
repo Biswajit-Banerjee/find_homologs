360821b4fe89b3bcae2e992eb751f4e8a780f950
Biswajit-Banerjee <biswajitbannerje@gmail.com>
2023-09-11 23:24:24 -0400

    modify find_homologs to 30% seq identify and 90% match length

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check for correct usage
-if [ $# -ne 3 ]; then
-	    echo "Usage: ./find_perfect_matches.sh <query file> <subject file> <output file>"
-	        exit 1
-fi
-
 # Input files
 query_file="$1"
 subject_file="$2"
@@ -16,14 +10,18 @@ output_file="$3"
 # Run BLAST and store the output in a temporary file
 blast_output=$(mktemp blast_output.XXXXXXXXXX)
 blastn -query "$query_file" -subject "$subject_file" -out "$blast_output" -outfmt "6 qseqid sseqid qlen slen length pident"
-
+i
 # Extract perfect matches (100% identity and equal length)
-awk '$5 == $3 && $6 == 100 {print $1}' "$blast_output" > "$output_file"
+#awk '$5 == $3 && $6 == 100 {print $1}' "$blast_output" > "$output_file"
+
+# Filter hits based on >30% sequence identity and >90% match length
+awk '$6 > 30 && ($5 / $3) > 0.9 {print $1}' "$blast_output" > "$output_file"
+
 
 # Count and print the number of perfect matches
 perfect_match_count=$(wc -l < "$output_file")
 echo "Number of perfect matches: $perfect_match_count"
 
 # Clean up temporary files
-echo "blast file: $blast_output"
+#echo "blast file: $blast_output"
 
